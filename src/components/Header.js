7530f6c4847388d66dfec36385139529454a450a
Jhonatas <jhonatasanic@hotmail.com>
2022-08-30 17:30:57 -0300

    refactor(Header): change container class name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <header
-        className="title_Header"
+        className="container-Header"
       >
         <h1>Sistema Solar</h1>
       </header>
