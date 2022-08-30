10b9f562989bfd098d48892bea8729f0528043cb
Jhonatas <jhonatasanic@hotmail.com>
2022-08-30 13:47:42 -0300

    adiciona header a pagina

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
-}
+};
 
 export default App;
