efde99be0dd592d7e710c10ccbda55cb02839a44
Jhonatas <jhonatasanic@hotmail.com>
2022-08-31 11:46:06 -0300

    feat(components): created component PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div>
+        <p>{ planetName }</p>
+        <img
+          src={ planetImage }
+          alt={`imagem do planeta ${ planetName }`}
+        />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
