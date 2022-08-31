13f37f6bb0efeb0cfe7282803dd3b1baa18f1e50
Jhonatas <jhonatasanic@hotmail.com>
2022-08-31 20:30:31 -0300

    feat(CSS/Missions): align cards in component Missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
 import missions from '../data/missions';
+import './Missions.css';
 
 class Missions extends Component {
   render() {
     return (
-      <div>
+      <div
+        className='container-main-Missions'
+      >
         <Title headline='Missões' />
-        { missions.map((mission, index) => 
-          <MissionCard
-            key={ index }
-            name={ mission.name }
-            year={ mission.year }
-            country={ mission.country }
-            destination={ mission.destination }
-          />
-        ) }
+        <div
+          className='container-Missions'
+        >
+          { missions.map((mission, index) => 
+            <MissionCard
+              key={ index }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          ) }
+        </div>
       </div>
     );
   }
