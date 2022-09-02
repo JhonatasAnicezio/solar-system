2d0e46425c70e381cdf1436fa30b79f5c92f28bf
Jhonatas <jhonatasanic@hotmail.com>
2022-09-02 11:40:09 -0300

    refactor(MissionCard): add props in state

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 import './MissionCard.css';
 
 class MissionCard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      year: '',
+      country: '',
+      destination: ''
+    };
+  }
+
   render() {
-    const { name, year, country, destination } = this.props;
+    const { name, year, country, destination } = this.state;
     return (
       <div
         className='container-CardM'
