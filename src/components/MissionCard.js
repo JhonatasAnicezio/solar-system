7e758b1cc0a246ec6a4745db516176bc2d1a174b
Jhonatas <jhonatasanic@hotmail.com>
2022-09-02 11:52:08 -0300

    fix(MissionCard): return the code to the current state

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -3,19 +3,8 @@ import PropTypes from 'prop-types';
 import './MissionCard.css';
 
 class MissionCard extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      year: '',
-      country: '',
-      destination: ''
-    };
-  }
-
   render() {
-    const { name, year, country, destination } = this.state;
+    const { name, year, country, destination } = this.props;
     return (
       <div
         className='container-CardM'
